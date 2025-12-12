3bcc8b08f9ab1e6adc77f2b42b2901d08ae3da5b
sanskarhere <sanskargupta693@gmail.com>
2025-12-12 20:05:40 +0530

    Initialised Repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='HeartDiseasePredictor',
+    version='0.0.1',
+    description='Predicts Heart Disease',
+    author='sanskarhere',
+    author_email='sanskarhere@gmail.com',
+    packages=find_packages()
+    )
